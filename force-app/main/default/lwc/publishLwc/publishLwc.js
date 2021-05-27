9aed80f793ebf7d9cb4b04f98f22ef0cb627c353
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-27 14:39:28 +0530

    Publisher LWC component

---
+++ force-app/main/default/lwc/publishLwc/publishLwc.js
@@ -0,0 +1,45 @@
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-27-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   05-27-2021   Vrushabh Uprikar   Initial Version
+**/
+import { LightningElement , track} from 'lwc';
+import getAccounts from '@salesforce/apex/returnAcc.getAccounts';
+import { createMessageContext, publish, releaseMessageContext } from 'lightning/messageService';
+import MessageChannel from '@salesforce/messageChannel/myFirstMessage__c';
+export default class PublishLwc extends LightningElement
+{
+    messageContext = createMessageContext();
+
+    @track accListData;
+    @track error;
+
+    connectedCallback()
+    {
+        getAccounts() // Loading Account details
+            .then(result => {
+                this.accListData = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    
+    }
+
+    handleClickhandleClick(event)
+    {
+        event.preventDefault(); // Prevent the default behavior of tags
+        const payload = {
+            recordId: event.target.dataset.value,
+            recordData: {
+                value: "Message From LWC"
+            }
+        };
+
+        
+        publish(this.messageContext, MessageChannel, payload);
+    }
+}
